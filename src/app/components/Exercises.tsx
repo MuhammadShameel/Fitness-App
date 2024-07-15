c4ef04c5e5f5303e00ca6349383ef42a0382477e
MuhammadShameel <mshameelkz@gmail.com>
2024-07-15 17:18:04 +0500

    exercise page created

--- src/app/components/Exercises.tsx
+++ src/app/components/Exercises.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Exercise } from "../types"; // Adjust the import path as needed
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+interface ExercisesProps {
+  exercises: Exercise[];
+  bodyPart: string;
+  setExercises: React.Dispatch<React.SetStateAction<Exercise[]>>;
+}
+
+const Exercises: React.FC<ExercisesProps> = ({ exercises }) => {
+  return (
+    <div>
+      <h1>Exercises are below</h1>
+      {exercises?.map((exercise) => (
+        <div key={exercise.id}>
+          <p>{exercise.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Exercises;
