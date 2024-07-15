da8918a68481cf7bbcb8fa907b03f3b8e5766219
MuhammadShameel <mshameelkz@gmail.com>
2024-07-15 14:16:28 +0500

    create horizontal scroll bar

---
+++ src/app/components/ScrollBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+import { ScrollMenu } from "react-horizontal-scrolling-menu";
+import "react-horizontal-scrolling-menu/dist/styles.css";
+
+import BodyPart from "./BodyPart";
+import LeftArrow from "./LeftArrow";
+import RightArrow from "./RightArrow";
+
+interface ScrollBarProps {
+  data: any[];
+  bodyPart: any;
+  setBodyPart: (bodyPart: any) => void;
+}
+
+const ScrollBar: React.FC<ScrollBarProps> = ({
+  data,
+  bodyPart,
+  setBodyPart,
+}) => {
+  return (
+    <ScrollMenu
+      wrapperClassName="scrollhost"
+      LeftArrow={LeftArrow}
+      RightArrow={RightArrow}
+    >
+      {data?.map((item: any) => (
+        <div
+          key={item.id || item}
+          itemID={item.id || item}
+          title={item.id || item}
+          className="my-40"
+        >
+          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        </div>
+      ))}
+    </ScrollMenu>
+  );
+};
+
+export default ScrollBar;
