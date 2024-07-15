f997b1772cad1b31ac8509e629f2e4f174fd62fe
MuhammadShameel <mshameelkz@gmail.com>
2024-07-15 14:17:23 +0500

    add all the page on home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
+"use client";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import SearchExercises from "./components/SearchExercises";
+import Exercises from "./components/Exercises";
+
+interface Exercise {
+  id: string;
+  name: string;
+  target: string;
+  equipment: string;
+  bodyPart: string;
+}
 
 export default function Home() {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
   return (
     <main>
       <Navbar />
-      <SearchExercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </main>
   );
 }
