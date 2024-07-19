971a6b9c33450d858184cfe4b1181ac9e6ef1312
MuhammadShameel <mshameelkz@gmail.com>
2024-07-19 21:08:01 +0500

    create exercise card

--- src/app/components/ExerciseCard.tsx
+++ src/app/components/ExerciseCard.tsx
@@ -7,28 +7,28 @@ const ExerciseCard = ({ exercise }: any) => {
     <main>
       <div className="flex flex-col items-center justify-center bg-white w-[270px] m-5 p-5 cursor-pointer shadow-md hover:shadow-lg transition-shadow duration-300">
         <Link href={`/exercise/${exercise.id}`} className="w-full">
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col ">
             <div className="relative w-full h-[200px]">
               <Image
                 src={exercise.gifUrl}
                 alt={exercise.name}
                 layout="fill"
                 objectFit="cover"
-                className="rounded-t-md"
+                className="rounded-t-md p-5"
               />
             </div>
-            <div className="flex justify-between">
-              <button className="p-2  text-white rounded-full capitalize bg-[#ffa9a9]">
+            <div className="flex justify-start mt-2">
+              <button className="p-2  text-white text-xs rounded-full capitalize bg-[#ffa9a9]">
                 {exercise.bodyPart}
               </button>
 
-              <button className="p-2 mx-1 text-white rounded-full capitalize bg-[#fcc757]">
+              <button className="p-2 mx-1 text-white text-xs  rounded-full capitalize bg-[#fcc757]">
                 {exercise.target}
               </button>
             </div>
 
-            <div className="mt-3 text-center">
-              <h3 className="text-xl font-medium text-gray-800 capitalize">
+            <div className="mt-3 text-start">
+              <h3 className="text-lg font-medium text-gray-800 capitalize">
                 {exercise.name}
               </h3>
             </div>
