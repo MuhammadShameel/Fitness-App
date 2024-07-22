7e2e0c6ea8178abad40859b5af37ad12689d5107
MuhammadShameel <mshameelkz@gmail.com>
2024-07-22 23:21:36 +0500

    fix styling

--- src/app/components/ExerciseVideo.tsx
+++ src/app/components/ExerciseVideo.tsx
@@ -3,30 +3,32 @@ import React from "react";
 const ExerciseVideo = ({ exerciseVideos, name }: any) => {
   return (
     <div className="container mx-auto">
-      <div className="mt-20 lg:mt-[203px] p-5">
+      <div className=" p-5">
         <h1 className="text-2xl lg:text-4xl font-bold text-black mb-8">
           Watch <span className="text-red-600 capitalize">{name}</span> exercise
           videos
         </h1>
-        <div className="flex flex-col lg:flex-row gap-0 lg:gap-[110px] justify-start flex-wrap items-center">
+        <div className="flex flex-wrap gap-6 justify-center items-center">
           {exerciseVideos?.slice(0, 3)?.map((item: any, index: any) => (
             <a
               key={index}
-              className="exercise-video"
+              className="exercise-video block max-w-xs bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-105"
               href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
               target="_blank"
               rel="noreferrer"
             >
               <img
-                className="rounded-tl-[20px]"
+                className="w-full h-48 object-cover"
                 src={item.video.thumbnails[0].url}
                 alt={item.video.title}
               />
-              <div>
-                <h2 className="text-lg lg:text-2xl font-semibold text-nowrap text-black">
+              <div className="p-4">
+                <h2 className="text-lg lg:text-xl font-semibold text-black truncate">
                   {item.video.title}
                 </h2>
-                <p className="text-sm text-black">{item.video.channelName}</p>
+                <p className="text-sm text-gray-600">
+                  {item.video.channelName}
+                </p>
               </div>
             </a>
           ))}
