b3e3fd178c1e2b00f2383ce7e0bb8b9246e97a23
MuhammadShameel <mshameelkz@gmail.com>
2024-07-23 15:52:09 +0500

    add a seprate scroll bar

--- src/app/components/SimilarExercise.tsx
+++ src/app/components/SimilarExercise.tsx
@@ -1,28 +1,24 @@
 import React from "react";
-import Scrollbar from "./ScrollBar";
-// import Loader from './Loader';
+import HorizontalScrollbar from "./HorizontalScrollbar";
+import Loading from "./SkeletonLoading";
 
 const SimilarExercises = ({
   targetMuscleExercises,
   equipmentExercises,
 }: any) => (
-  <div className="mt-0 lg:mt-24">
+  <div className="container mx-auto mt-0 lg:mt-24">
     <h1 className="text-black font-bold text-2xl lg:text-4xl mb-8 ml-5">
       Similar <span className="text-red-600 capitalize">Target Muscle</span>{" "}
       exercises
     </h1>
     <div className="flex p-2 relative">
       {targetMuscleExercises.length !== 0 ? (
-        <Scrollbar
+        <HorizontalScrollbar
           data={targetMuscleExercises}
           bodyPart={undefined}
-          setBodyPart={function (bodyPart: any): void {
-            throw new Error("Function not implemented.");
-          }}
         />
       ) : (
-        // <Loader />
-        <p>loading.....</p>
+        <Loading />
       )}
     </div>
     <h1 className="text-black font-bold text-2xl lg:text-4xl mb-8 ml-5 mt-14 lg:mt-24">
@@ -31,7 +27,7 @@ const SimilarExercises = ({
     </h1>
     <div className="flex p-2 relative">
       {equipmentExercises.length !== 0 ? (
-        <Scrollbar
+        <HorizontalScrollbar
           data={equipmentExercises}
           bodyPart={undefined}
           setBodyPart={function (bodyPart: any): void {
@@ -39,8 +35,7 @@ const SimilarExercises = ({
           }}
         />
       ) : (
-        // <Loader />
-        <p>loading.....</p>
+        <Loading />
       )}
     </div>
   </div>
